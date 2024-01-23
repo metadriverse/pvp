06d5c0bd208400f06b06d692517ba8e1bbf979f5
pengzhenghao <pzh@cs.ucla.edu>
2024-01-23 14:05:31 -0800

    formalize the MD env (almost)

--- pvp/experiments/metadrive/human_in_the_loop_env.py
+++ pvp/experiments/metadrive/human_in_the_loop_env.py
@@ -10,117 +10,111 @@ from metadrive.utils.math import safe_clip
 
 ScreenMessage.SCALE = 0.1
 
+HUMAN_IN_THE_LOOP_ENV_CONFIG = {
+    # Environment setting:
+    "out_of_route_done": True,  # Raise done if out of route.
+    "num_scenarios": 50,  # There are totally 50 possible maps.
+    "start_seed": 100,  # We will use the map 100~150 as the default training environment.
+    # "random_spawn": True,  # The spawn positions of ego car is randomized.  # TODO: Remove this.
+    "traffic_density": 0.06,
+
+    # Reward and cost setting:
+    "cost_to_reward": True,  # Cost will be negated and added to the reward. Useless in PVP.
+    "cos_similarity": True,  # If True, the takeover cost will be the cos sim between human/agent actions.
+
+    # Set up the control device. Default to use keyboard with the pop-up interface.
+    "manual_control": True,
+    "agent_policy": TakeoverPolicy,
+    "controller": "keyboard",  # Selected from [keyboard, xbox, steering_wheel].
+    "only_takeover_start_cost": True,  # If True, only return a cost when takeover starts. Useless in PVP.
+
+    # Visualization
+    "vehicle_config": {
+        "show_dest_mark": True,  # Show the destination in a cube.
+        "show_line_to_dest": True,  # Show the line to the destination.
+        "show_line_to_navi_mark": True,  # Show the line to next navigation checkpoint.
+    }
+}
+
 
 class HumanInTheLoopEnv(SafeMetaDriveEnv):
     """
-    This Env depends on the new version of MetaDrive
+    Human-in-the-loop Env Wrapper for the Safety Env in MetaDrive.
+    Add code for computing takeover cost and add information to the interface.
     """
-
-    _takeover_recorder = deque(maxlen=2000)
-
     total_steps = 0
+    total_takeover_cost = 0
+    takeover = False
+    takeover_recorder = deque(maxlen=2000)
+    agent_action = None
+    in_pause = False
 
     def default_config(self):
         config = super(HumanInTheLoopEnv, self).default_config()
-        config.update(
-            {
-                # TODO: add more docs for the config here.
-                "num_scenarios": 50,
-                "start_seed": 100,
-
-                # Environment setting:
-
-                # Controller:
-
-                # Reward and cost setting:
-                "cost_to_reward": True,
-                "traffic_density": 0.06,
-                "manual_control": False,
-                "out_of_route_done": True,
-                "controller": "keyboard",  # Selected from [keyboard, xbox, steering_wheel].
-                "agent_policy": TakeoverPolicy,
-                "only_takeover_start_cost": True,
-                "main_exp": True,
-                "random_spawn": True,
-                "cos_similarity": True,
-                "in_replay": False,
-
-                # Visualization
-                "vehicle_config": {
-                    "show_dest_mark": True,
-                    "show_line_to_dest": True,
-                    "show_line_to_navi_mark": True,
-                }
-            },
-            allow_add_new_key=True
-        )
+        config.update(HUMAN_IN_THE_LOOP_ENV_CONFIG, allow_add_new_key=True)
         return config
 
     def reset(self, *args, **kwargs):
-        self.in_stop = False
-        self.t_o = False
-        self.total_takeover_cost = 0
-        self.input_action = None
+        self.in_pause = False
+        self.takeover = False
+        # self.total_takeover_cost = 0
+        self.agent_action = None
         obs, info = super(HumanInTheLoopEnv, self).reset(*args, **kwargs)
-        if self.config["random_spawn"]:
-            self.config["vehicle_config"]["spawn_lane_index"] = (
-                FirstPGBlock.NODE_1, FirstPGBlock.NODE_2, self.engine.np_random.randint(3)
-            )
+        # if self.config["random_spawn"]:
+        #     self.config["vehicle_config"]["spawn_lane_index"] = (
+        #         FirstPGBlock.NODE_1, FirstPGBlock.NODE_2, self.engine.np_random.randint(3)
+        #     )
 
         # The training code is for older version of gym, so we discard the additional info from the reset.
         return obs
 
     def _get_step_return(self, actions, engine_info):
+        """Compute takeover cost here."""
         o, r, tm, tc, engine_info = super(HumanInTheLoopEnv, self)._get_step_return(actions, engine_info)
-
-        # TODO(pzh): Double check what is this
-        # if self.config["in_replay"]:
-        #     return o, r, d, engine_info
-
         d = tm or tc
 
-        controller = self.engine.get_policy(self.vehicle.id)
-        last_t = self.t_o
-        self.t_o = controller.takeover if hasattr(controller, "takeover") else False
-        engine_info["takeover_start"] = True if not last_t and self.t_o else False
-        engine_info["takeover"] = self.t_o and not engine_info["takeover_start"]
-        condition = engine_info["takeover_start"] if self.config["only_takeover_start_cost"] else self.t_o
-
+        shared_control_policy = self.engine.get_policy(self.agent.id)
+        last_t = self.takeover
+        self.takeover = shared_control_policy.takeover if hasattr(shared_control_policy, "takeover") else False
+        engine_info["takeover_start"] = True if not last_t and self.takeover else False
+        engine_info["takeover"] = self.takeover and not engine_info["takeover_start"]
+        condition = engine_info["takeover_start"] if self.config["only_takeover_start_cost"] else self.takeover
         if not condition:
-            self.total_takeover_cost += 0
             engine_info["takeover_cost"] = 0
         else:
             cost = self.get_takeover_cost(engine_info)
             self.total_takeover_cost += cost
             engine_info["takeover_cost"] = cost
-
         engine_info["total_takeover_cost"] = self.total_takeover_cost
         engine_info["native_cost"] = engine_info["cost"]
         engine_info["total_native_cost"] = self.episode_cost
+
         return o, r, d, engine_info
 
     def _is_out_of_road(self, vehicle):
+        """Out of road condition"""
         ret = (not vehicle.on_lane) or vehicle.crash_sidewalk
         if self.config["out_of_route_done"]:
             ret = ret or vehicle.out_of_route
         return ret
 
     def step(self, actions):
-        self.input_action = copy.copy(actions)
+        """Add additional information to the interface."""
+        self.agent_action = copy.copy(actions)
         ret = super(HumanInTheLoopEnv, self).step(actions)
-        while self.in_stop:
+        while self.in_pause:
             self.engine.taskMgr.step()
 
-        self._takeover_recorder.append(self.t_o)
-        if self.config["use_render"] and self.config["main_exp"] and not self.config["in_replay"]:
+        self.takeover_recorder.append(self.takeover)
+        if self.config["use_render"]:  # and self.config["main_exp"]: #and not self.config["in_replay"]:
             super(HumanInTheLoopEnv, self).render(
                 text={
                     "Total Cost": round(self.episode_cost, 3),
                     "Takeover Cost": round(self.total_takeover_cost, 3),
-                    "Takeover": self.t_o,
+                    "Takeover": self.takeover,
                     "COST": ret[-1]["takeover_cost"],
                     "Total Step": self.total_steps,
-                    "Takeover Rate": "{:.3f}%".format(np.mean(np.array(self._takeover_recorder) * 100)),
+                    "Takeover Rate": "{:.3f}%".format(np.mean(np.array(self.takeover_recorder) * 100)),
                     "Pause (Press E)": "",
                 }
             )
@@ -130,38 +124,34 @@ class HumanInTheLoopEnv(SafeMetaDriveEnv):
         return ret
 
     def stop(self):
-        self.in_stop = not self.in_stop
+        """Toggle pause."""
+        self.in_pause = not self.in_pause
 
     def setup_engine(self):
+        """Introduce additional key 'e' to the interface."""
         super(HumanInTheLoopEnv, self).setup_engine()
         self.engine.accept("e", self.stop)
 
     def get_takeover_cost(self, info):
+        """Return the takeover cost when intervened."""
         if not self.config["cos_similarity"]:
             return 1
-
         takeover_action = safe_clip(np.array(info["raw_action"]), -1, 1)
-        agent_action = safe_clip(np.array(self.input_action), -1, 1)
-
+        agent_action = safe_clip(np.array(self.agent_action), -1, 1)
         multiplier = (agent_action[0] * takeover_action[0] + agent_action[1] * takeover_action[1])
         divident = np.linalg.norm(takeover_action) * np.linalg.norm(agent_action)
         if divident < 1e-6:
             cos_dist = 1.0
         else:
             cos_dist = multiplier / divident
-
         return 1 - cos_dist
 
 
 if __name__ == "__main__":
-    env = HumanInTheLoopEnv(
-        {
-            "manual_control": True,
-            "disable_model_compression": True,
-            "use_render": True,
-            "main_exp": True
-        }
-    )
+    env = HumanInTheLoopEnv({
+        "manual_control": True,
+        "use_render": True,
+    })
     env.reset()
     while True:
         _, _, done, _ = env.step([0, 0])
