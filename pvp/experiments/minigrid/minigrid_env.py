21f2d08ece2b6aa28024de2256c2b49b937d497b
pengzhenghao <pzh@cs.ucla.edu>
2024-01-24 21:33:16 -0800

    minigrid seems to be working, need further check

--- pvp/experiments/minigrid/minigrid_env.py
+++ pvp/experiments/minigrid/minigrid_env.py
@@ -1,70 +1,156 @@
 """
-TODO: Rewrite this file with latest minigrid.
+Borrowed part of the code from:
+https://github.com/Farama-Foundation/Minigrid/blob/master/minigrid/manual_control.py
 """
-import cv2
-import gym
-import matplotlib.pyplot as plt
+# import gym
+import gymnasium as gym
 import numpy as np
-import torch
-from gym import Wrapper
-from minigrid.core.constants import OBJECT_TO_IDX, COLOR_TO_IDX
+# from gym import Wrapper
 
-from pvp.experiments.minigrid.rendering import Window
+import pygame
+
+from minigrid.envs import EmptyEnv as NativeEmptyEnv
+
+ADDITIONAL_HEIGHT = 200
+RULE_WIDTH = 5
 
 
-class GrayScaleWrapper(gym.ObservationWrapper):
-    def __init__(self, env: gym.Env):
-        gym.ObservationWrapper.__init__(self, env)
-        self.observation_space = gym.spaces.Box(
-            low=0,
-            high=255,
-            shape=(env.observation_space.shape[0], env.observation_space.shape[1], 1),
-            dtype=env.observation_space.dtype
+def new_render(self):
+    img = self.get_frame(self.highlight, self.tile_size, self.agent_pov)
+
+    if self.render_mode == "human":
+        img = np.transpose(img, axes=(1, 0, 2))
+        if self.render_size is None:
+            self.render_size = img.shape[:2]
+        if self.window is None:
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode(
+                (self.screen_size, self.screen_size + ADDITIONAL_HEIGHT)
+            )
+            pygame.display.set_caption("minigrid")
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+        surf = pygame.surfarray.make_surface(img)
+
+        # Create background with mission description
+        offset = surf.get_size()[0] * 0.1
+        # offset = 32 if self.agent_pov else 64
+        bg = pygame.Surface(
+            (int(surf.get_size()[0] + offset), int(surf.get_size()[1] + offset))
         )
+        bg.convert()
+        bg.fill((255, 255, 255))
+        bg.blit(surf, (offset / 2, 0))
 
-    def observation(self, frame):
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        return frame[:, :, None]
+        bg = pygame.transform.smoothscale(bg, (self.screen_size, self.screen_size))
+
+        # PZH: Larger font size
+        # font_size = 22
+        font_size = 50
+
+        text = self.mission
+        font = pygame.freetype.SysFont(pygame.font.get_default_font(), font_size)
+        text_rect = font.get_rect(text, size=font_size)
+        text_rect.center = bg.get_rect().center
+        text_rect.y = bg.get_height() - font_size * 1.3
+        font.render_to(bg, text_rect, text, size=font_size)
+        # self.window.fill((255, 255, 0))
+
+        self.window.blit(bg, (0, 0))
+
+        # ===== PZH: We additionally print something here. =====
+        if self.additional_text:
+            additional_text_bg = pygame.Surface(
+                (self.screen_size, ADDITIONAL_HEIGHT)
+            )
+            additional_text_bg.fill((255, 255, 255))
+            # Split the text into lines
+            lines = self.additional_text.split('\n')
+            # Calculate the starting y position
+            start_y = font_size * 0.5
+            for i, line in enumerate(lines):
+                # Calculate the y position for each line
+                y_position = start_y + (font_size * 1.5 * i)
+                # Create a rectangle for the line
+                text_rect = font.get_rect(line, size=font_size)
+                text_rect.centerx = additional_text_bg.get_rect().centerx
+                text_rect.y = y_position
+                # Render the line to the background surface
+                font.render_to(additional_text_bg, text_rect, line, size=font_size)
+            self.window.blit(additional_text_bg, (0, self.screen_size + RULE_WIDTH))
+        # ===== PZH: We additionally print something here. =====
+
+        pygame.event.pump()
+        self.clock.tick(self.metadata["render_fps"])
+        pygame.display.flip()
+
+    elif self.render_mode == "rgb_array":
+        return img
 
 
-class MinigridWrapper(Wrapper):
-    def __init__(self, env, enable_human=False, enable_render=False):
+class EmptyEnv(NativeEmptyEnv):
+    additional_text = ""
+
+    def update_additional_text(self, text):
+        self.additional_text = text
+
+    def render(self):
+        return new_render(self)
+
+
+class MiniGridEmpty6x6(EmptyEnv):
+    """
+    Following:
+        register(
+            id="MiniGrid-Empty-6x6-v0",
+            entry_point="minigrid.envs:EmptyEnv",
+            kwargs={"size": 6},
+        )
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, size=6, render_mode="human", screen_size=1000, **kwargs)
+
+
+class MinigridWrapper(gym.Wrapper):
+    def __init__(self, env):
         super(MinigridWrapper, self).__init__(env=env)
         self.total_cost = 0
         self.takeover = False
+        self.use_render = self.enable_human = env.render_mode == "human"
         self.keyboard_action = None
         self.valid_key_press = False
-
-        self.enable_human = enable_human
-        if enable_human:
-            assert enable_render
-        if enable_render:
-            self.window = Window('gym_minigrid - ' + self.env.spec.id)
-            if enable_human:
-                self.window.reg_key_handler(self.discrete_key_detect)
-        else:
-            self.window = None
-
-        # PZH: We can simply leave three useful action dimensions here.
-        #  but for consistency for future complex grid env, we just leave the nextli
+        # TODO(PZH): We can simply leave only three useful action dimensions here.
         # self.action_space = gym.spaces.Discrete(3)
 
     def step(self, a):
         self.update_caption(a)
 
         if self.enable_human:
-            button = plt.waitforbuttonpress(0.03)
-            while (not button) or (not self.valid_key_press):
-                button = plt.waitforbuttonpress(0.03)
+            should_break = False
+            while not should_break:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        # TODO: Deal with this.
+                        self.close()
+                        should_break = True
+                        break
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            self.close()
+                        else:
+                            event.key = pygame.key.name(int(event.key))
+                            self.discrete_key_detect(event)
+                        should_break = True
+                        break
 
+        # TODO: Check all here.
         should_takeover = self.keyboard_action is not None
-        # if should_takeover and a != self.keyboard_action:
-        #     cost = 0.1
-        # else:
-        #     cost = 0
         cost = 0
         behavior_action = self.keyboard_action if should_takeover else a
-        o, r, d, i = super(MinigridWrapper, self).step(behavior_action)
+        o, r, tm, tc, i = super(MinigridWrapper, self).step(behavior_action)
+        d = tm or tc
         takeover_start = should_takeover and not self.takeover
         i["cost"] = cost
         i["total_cost"] = self.total_cost
@@ -76,20 +162,18 @@ class MinigridWrapper(Wrapper):
         i["is_success"] = i["success"] = True if r > 0.0 else False
         self.takeover = should_takeover
         self.valid_key_press = False  # refresh
-
         self.update_caption(None)  # Set caption to "waiting"
-
         return o, r, d, i
 
     def reset(self):
         self.total_cost = 0
         self.takeover = False
-        ret = super(MinigridWrapper, self).reset()
-
-        if self.window is not None:
-            self.window.set_caption("Reset!")
-            self.redraw()
-
+        self.keyboard_action = None
+        # ret = super(MinigridWrapper, self).reset()
+        ret = self.env.reset()
+        if self.use_render:
+            pygame.display.set_caption("Reset!")
+            self.env.render()
         return ret
 
     def discrete_key_detect(self, event):
@@ -106,43 +190,30 @@ class MinigridWrapper(Wrapper):
         elif event.key == " " or event.key == "down":  # Space/Down means allowing agent action!
             self.valid_key_press = True
             self.keyboard_action = None
-
         elif event.key == "p":
             # Pick up an object
             self.valid_key_press = True
             self.keyboard_action = self.env.actions.pickup
-
         elif event.key == "d":
             # Drop an object
             self.valid_key_press = True
             self.keyboard_action = self.env.actions.drop
-
         elif event.key == "t":
             # Toggle/activate an object
             self.valid_key_press = True
             self.keyboard_action = self.env.actions.toggle
-
         elif event.key == "x":
             # Done completing task
             self.valid_key_press = True
             self.keyboard_action = self.env.actions.done
-
         else:
             self.valid_key_press = False
 
-    def redraw(self):
-        if self.window is None:
-            return
-
-        img = self.env.render('rgb_array', tile_size=32)
-        self.window.show_img(img)
-
     def update_caption(self, agent_action=None):
-        if self.window is None:
+        if not self.use_render:
             return
-
         if agent_action is None:
-            self.window.set_caption("Waiting ...".format(agent_action))
+            self.update_additional_text("Waiting agent action...".format(agent_action))
         else:
             if agent_action < 7:
                 agent_action = {
@@ -156,135 +227,18 @@ class MinigridWrapper(Wrapper):
                 }[agent_action]
             else:
                 agent_action = "Invalid action: {}".format(agent_action)
-            self.window.set_caption("Agent action: {}".format(agent_action))
-
-        img = self.env.render('rgb_array', tile_size=32)
-        self.window.show_img(img)
-
-        # We have to redraw image after changing the caption
-        self.redraw()
-
-
-# Copied from: https://github.com/facebookresearch/impact-driven-exploration/blob/877c4ea530cc0ca3902211dba4e922bf8c3ce276/src/env_utils.py#L38
-
-
-class FullyObsCustomWrapper(gym.Wrapper):
-    def __init__(self, env, fix_seed=False, env_seed=1):
-        super(FullyObsCustomWrapper, self).__init__(env)
-        self.episode_return = None
-        self.episode_step = None
-        self.episode_win = None
-        self.fix_seed = fix_seed
-        self.env_seed = env_seed
-
-    def get_partial_obs(self):
-        return self.gym_env.env.env.gen_obs()['image']
-
-    def initial(self):
-        initial_reward = torch.zeros(1, 1)
-        # self.episode_return = torch.zeros(1, 1)
-        # self.episode_step = torch.zeros(1, 1, dtype=torch.int32)
-        # self.episode_win = torch.zeros(1, 1, dtype=torch.int32)
-        initial_done = torch.ones(1, 1, dtype=torch.uint8)
-        if self.fix_seed:
-            self.gym_env.seed(seed=self.env_seed)
-        initial_frame = _format_observation(self.gym_env.reset())
-        partial_obs = _format_observation(self.get_partial_obs())
-
-        if self.gym_env.env.env.carrying:
-            carried_col, carried_obj = torch.LongTensor(
-                [[COLOR_TO_IDX[self.gym_env.env.env.carrying.color]]]
-            ), torch.LongTensor([[OBJECT_TO_IDX[self.gym_env.env.env.carrying.type]]])
-        else:
-            carried_col, carried_obj = torch.LongTensor([[5]]), torch.LongTensor([[1]])
-
-        return dict(
-            frame=initial_frame,
-            reward=initial_reward,
-            done=initial_done,
-            # episode_return=self.episode_return,
-            # episode_step=self.episode_step,
-            # episode_win=self.episode_win,
-            carried_col=carried_col,
-            carried_obj=carried_obj,
-            partial_obs=partial_obs
-        )
-
-    def step(self, action):
-        frame, reward, done, _ = self.gym_env.step(action.item())
-
-        self.episode_step += 1
-        episode_step = self.episode_step
-
-        self.episode_return += reward
-        episode_return = self.episode_return
-
-        if done and reward > 0:
-            self.episode_win[0][0] = 1
-        else:
-            self.episode_win[0][0] = 0
-        episode_win = self.episode_win
-
-        if done:
-            if self.fix_seed:
-                self.gym_env.seed(seed=self.env_seed)
-            frame = self.gym_env.reset()
-            # self.episode_return = torch.zeros(1, 1)
-            # self.episode_step = torch.zeros(1, 1, dtype=torch.int32)
-            # self.episode_win = torch.zeros(1, 1, dtype=torch.int32)
-
-        frame = _format_observation(frame)
-        reward = torch.tensor(reward).view(1, 1)
-        done = torch.tensor(done).view(1, 1)
-        partial_obs = _format_observation(self.get_partial_obs())
-
-        if self.gym_env.env.env.carrying:
-            carried_col, carried_obj = torch.LongTensor(
-                [[COLOR_TO_IDX[self.gym_env.env.env.carrying.color]]]
-            ), torch.LongTensor([[OBJECT_TO_IDX[self.gym_env.env.env.carrying.type]]])
-        else:
-            carried_col, carried_obj = torch.LongTensor([[5]]), torch.LongTensor([[1]])
-
-        return dict(
-            frame=frame,
-            reward=reward,
-            done=done,
-            episode_return=episode_return,
-            episode_step=episode_step,
-            episode_win=episode_win,
-            carried_col=carried_col,
-            carried_obj=carried_obj,
-            partial_obs=partial_obs
-        )
-
-    def get_full_obs(self):
-        env = self.gym_env.unwrapped
-        full_grid = env.grid.encode()
-        full_grid[env.agent_pos[0]][env.agent_pos[1]
-                                    ] = np.array([OBJECT_TO_IDX['agent'], COLOR_TO_IDX['red'], env.agent_dir])
-        return full_grid
+            self.update_additional_text("Agent action: {}".format(agent_action))
+        if self.use_render:
+            self.env.render()
 
     def close(self):
-        self.gym_env.close()
+        self.env.close()
+        pygame.quit()
+        raise KeyboardInterrupt()
 
 
 if __name__ == '__main__':
-
-    # Preliminary:
-    #  pip install gym-minigrid
-
-    # Github repo: Farama-Foundation/gym-minigrid
-    # Key idea: stop at each step!
-    # Keys:
-    #   up arrow - move forward
-    #   left/right arrows - rotate left/right
-    #   space - allow agent action
-
-    env_name = 'MiniGrid-Empty-6x6-v0'
-
-    # Don't forget this wrapper!
-    env = MinigridWrapper(gym.make(env_name))
-
+    env = MinigridWrapper(MiniGridEmpty6x6())
     env.reset()
     while True:
         o, r, d, i = env.step(env.action_space.sample())
