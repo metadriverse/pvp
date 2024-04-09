ee20c3c21398973fe29fbb4887d5c35c2fb4cdad
pengzhenghao <pzh@cs.ucla.edu>
2024-04-09 10:42:21 -0700

    fix

--- scripts/0409_pvp_egpo.sh
+++ scripts/0409_pvp_egpo.sh
@@ -1,23 +1,17 @@
-nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
---exp_name=pvp-metadrive-fake-free0.95 \
---wandb \
---wandb_project=pvp2024 \
---wandb_team=drivingforce \
---seed=0 \
-> 0409_pvp_egpo_seed0.log 2>&1 &
+#!/bin/bash
 
-nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
---exp_name=pvp-metadrive-fake-free0.95 \
---wandb \
---wandb_project=pvp2024 \
---wandb_team=drivingforce \
---seed=100 \
-> 0409_pvp_egpo_seed100.log 2>&1 &
+# Define the seeds for each GPU
+seeds=(0 100 200 300 400 500 600 700)
 
-nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
---exp_name=pvp-metadrive-fake-free0.95 \
---wandb \
---wandb_project=pvp2024 \
---wandb_team=drivingforce \
---seed=200 \
-> 0409_pvp_egpo_seed200.log 2>&1 &
+# Loop over each GPU
+for i in {0..7}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
+    --exp_name=pvp-metadrive-fake-free0.95 \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    > "0409_pvp_egpo_seed${seeds[$i]}.log" 2>&1 &
+done
