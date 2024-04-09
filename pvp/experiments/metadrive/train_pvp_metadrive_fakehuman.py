e71ad3cfd6acdce82e4176a2bf163db97e3bef9f
pengzhenghao <pzh@cs.ucla.edu>
2024-04-09 10:50:57 -0700

    fun

--- pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py
+++ pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
 
     # ===== Set up some arguments =====
     # control_device = args.device
-    experiment_batch_name = "{}".format(args.exp_name)
+    experiment_batch_name = "{}_freelevel{}".format(args.exp_name, args.free_level)
     seed = args.seed
     trial_name = "{}_{}_{}".format(experiment_batch_name, get_time_str(), uuid.uuid4().hex[:8])
     print("Trial name is set to: ", trial_name)
--- scripts/0409_pvp_egpo.sh
+++ scripts/0409_pvp_egpo.sh
@@ -8,11 +8,72 @@ for i in {0..7}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
-    --exp_name=pvp-metadrive-fake-free0.95 \
+    --exp_name=pvp-metadrive-fake \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
     --seed=${seeds[$i]} \
     --free_level=0.90 \
     > "0409_pvp_egpo_freelevel0.9_seed${seeds[$i]}.log" 2>&1 &
+done
+
+
+# Loop over each GPU
+for i in {0..7}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
+    --exp_name=pvp-metadrive-fake \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.99 \
+    > "0409_pvp_egpo_freelevel0.99_seed${seeds[$i]}.log" 2>&1 &
+done
+
+
+# Loop over each GPU
+for i in {0..7}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
+    --exp_name=pvp-metadrive-fake \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.95 \
+    > "0409_pvp_egpo_freelevel0.95_seed${seeds[$i]}.log" 2>&1 &
+done
+
+
+# Loop over each GPU
+for i in {0..7}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
+    --exp_name=pvp-metadrive-fake \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.85 \
+    > "0409_pvp_egpo_freelevel0.85_seed${seeds[$i]}.log" 2>&1 &
+done
+
+
+
+# Loop over each GPU
+for i in {0..7}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
+    --exp_name=pvp-metadrive-fake \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.8 \
+    > "0409_pvp_egpo_freelevel0.8_seed${seeds[$i]}.log" 2>&1 &
 done
