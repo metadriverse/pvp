59365275adb16e166bd8bb464a001f93719a93f6
pengzhenghao <pzh@cs.ucla.edu>
2024-04-09 21:30:25 -0700

    update script

--- scripts/0409_cpl_metadrive_fake.sh
+++ scripts/0409_cpl_metadrive_fake.sh
@@ -3,6 +3,7 @@
 # Define the seeds for each GPU
 seeds=(0 100 200 300 400 500 600 700)
 
+
 # Loop over each GPU
 for i in {0..7}
 do
@@ -13,8 +14,8 @@ do
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
     --seed=${seeds[$i]} \
-    --free_level=0.90 \
-    > "0409_cpl_egpo_freelevel0.9_seed${seeds[$i]}.log" 2>&1 &
+    --free_level=0.5 \
+    > "0409_cpl_egpo_freelevel0.5_seed${seeds[$i]}.log" 2>&1 &
 done
 
 
@@ -28,8 +29,8 @@ do
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
     --seed=${seeds[$i]} \
-    --free_level=0.99 \
-    > "0409_cpl_egpo_freelevel0.99_seed${seeds[$i]}.log" 2>&1 &
+    --free_level=0.0 \
+    > "0409_cpl_egpo_freelevel0_seed${seeds[$i]}.log" 2>&1 &
 done
 
 
@@ -37,43 +38,12 @@ done
 for i in {0..7}
 do
     CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_cpl_metadrive_fakehuman.py \
-    --exp_name=cpl-metadrive-fake \
+    nohup python pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py \
+    --exp_name=pvp-metadrive-fake \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
     --seed=${seeds[$i]} \
     --free_level=0.95 \
-    > "0409_cpl_egpo_freelevel0.95_seed${seeds[$i]}.log" 2>&1 &
-done
-
-
-# Loop over each GPU
-for i in {0..7}
-do
-    CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_cpl_metadrive_fakehuman.py \
-    --exp_name=cpl-metadrive-fake \
-    --wandb \
-    --wandb_project=pvp2024 \
-    --wandb_team=drivingforce \
-    --seed=${seeds[$i]} \
-    --free_level=0.85 \
-    > "0409_cpl_egpo_freelevel0.85_seed${seeds[$i]}.log" 2>&1 &
-done
-
-
-
-# Loop over each GPU
-for i in {0..7}
-do
-    CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_cpl_metadrive_fakehuman.py \
-    --exp_name=cpl-metadrive-fake \
-    --wandb \
-    --wandb_project=pvp2024 \
-    --wandb_team=drivingforce \
-    --seed=${seeds[$i]} \
-    --free_level=0.8 \
-    > "0409_cpl_egpo_freelevel0.8_seed${seeds[$i]}.log" 2>&1 &
+    > "0409_pvp_egpo_freelevel0.95_seed${seeds[$i]}.log" 2>&1 &
 done
