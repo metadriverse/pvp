c1fd64ddef342489b312eb25f8723f339546288e
pengzhenghao <pzh@cs.ucla.edu>
2024-04-16 13:18:56 -0700

    up

--- scripts/0411_pvpes.sh
+++ scripts/0411_pvpes.sh
@@ -5,11 +5,11 @@ seeds=(0 100 200 300 400 500 600 700)
 
 
 # Loop over each GPU
-for i in {0..3}
+for i in {4..7}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_pvpes_metadrive_fakehuman.py \
-    --exp_name=pvpes-metadrive-reward_0_for_negative-use_weighted_reward \
+    --exp_name=pvpes-metadrive-reward_0_for_negative-reward_1_for_all \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
@@ -18,9 +18,9 @@ do
     --no_done_for_positive=False \
     --reward_0_for_positive=False \
     --reward_n2_for_intervention=False \
-    --reward_1_for_all=False \
+    --reward_1_for_all=True \
     --reward_0_for_negative=True \
-    --use_weighted_reward=True \
-    > "0412-pvpes-reward_0_for_negative-use_weighted_reward-seed${seeds[$i]}.log" 2>&1 &
+    --use_weighted_reward=False \
+    > "0412-pvpes-reward_0_for_negative-reward_1_for_all-seed${seeds[$i]}.log" 2>&1 &
 done
 
