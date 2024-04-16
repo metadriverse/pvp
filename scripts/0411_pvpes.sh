4e3933cfe32523594f1e76540b82fea62cc8c76f
pengzhenghao <pzh@cs.ucla.edu>
2024-04-16 11:08:04 -0700

    up

--- scripts/0411_pvpes.sh
+++ scripts/0411_pvpes.sh
@@ -3,26 +3,6 @@
 # Define the seeds for each GPU
 seeds=(0 100 200 300)
 
-# Loop over each GPU
-for i in {0..3}
-do
-    CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_pvpes_metadrive_fakehuman.py \
-    --exp_name=pvpes-metadrive-reward_1_for_all \
-    --wandb \
-    --wandb_project=pvp2024 \
-    --wandb_team=drivingforce \
-    --seed=${seeds[$i]} \
-    --free_level=0.95 \
-    --no_done_for_positive=False \
-    --reward_0_for_positive=False \
-    --reward_n2_for_intervention=False \
-    --reward_1_for_all=True \
-    --reward_0_for_negative=False \
-    > "0412-pvpes-reward_1_for_all-seed${seeds[$i]}.log" 2>&1 &
-done
-
-
 
 # Loop over each GPU
 for i in {4..7}
