2d24d70f8ee8ccdd38eca0327d6ffdf673ed47d1
pengzhenghao <pzh@cs.ucla.edu>
2024-04-24 13:32:01 -0700

    add

--- scripts/0424_cplppo.sh
+++ scripts/0424_cplppo.sh
@@ -5,53 +5,36 @@ seeds=(0 100 200 300 400 500 600 700)
 
 
 # Loop over each GPU
-for i in {0..1}
+for i in {2..4}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-chunkadv-num_comparisons32 \
+    --exp_name=cplppo-chunkadv-num_steps_per_chunk32 \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
     --seed=${seeds[$i]} \
     --free_level=0.95 \
     --use_chunk_adv=True \
-    --num_comparisons=32 \
-    > "0424-cplppo-use_chunk_adv-num_comparisons32-seed${seeds[$i]}.log" 2>&1 &
+    --num_steps_per_chunk=32 \
+    > "0424-cplppo-use_chunk_adv-num_steps_per_chunk32-seed${seeds[$i]}.log" 2>&1 &
 done
 
 
 # Loop over each GPU
-for i in {0..1}
+for i in {0..2}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-chunkadv-num_comparisons128 \
+    --exp_name=cplppo-chunkadv-num_steps_per_chunk128 \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
     --seed=${seeds[$i]} \
     --free_level=0.95 \
     --use_chunk_adv=True \
-    --num_comparisons=128 \
-    > "0424-cplppo-use_chunk_adv-num_comparisons128-seed${seeds[$i]}.log" 2>&1 &
+    --num_steps_per_chunk=128 \
+    > "0424-cplppo-use_chunk_adv-num_steps_per_chunk128-seed${seeds[$i]}.log" 2>&1 &
 done
 
-
-
-# Loop over each GPU
-for i in {0..1}
-do
-    CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-chunkadv-num_comparisons256 \
-    --wandb \
-    --wandb_project=pvp2024 \
-    --wandb_team=drivingforce \
-    --seed=${seeds[$i]} \
-    --free_level=0.95 \
-    --use_chunk_adv=True \
-    --num_comparisons=256 \
-    > "0424-cplppo-use_chunk_adv-num_comparisons256-seed${seeds[$i]}.log" 2>&1 &
-done
 
