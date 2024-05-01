2e05377345f2d5925caeecef364842ff69ac98b9
pengzhenghao <pzh@cs.ucla.edu>
2024-05-01 13:39:32 -0700

    grid search

--- scripts/0430_cplppo.sh
+++ scripts/0430_cplppo.sh
@@ -9,7 +9,7 @@ for i in {0..1}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias0.0-add_loss_5=False-deter-top_factor=0.5 \
+    --exp_name=cplppo-cpl_bias=0.25-add_loss_5=False-deter-top_factor=0.5 \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
@@ -17,7 +17,7 @@ do
     --free_level=0.95 \
     --use_chunk_adv=True \
     --num_steps_per_chunk=64 \
-    --cpl_bias=0.0 \
+    --cpl_bias=0.25 \
     --num_comparisons=-1 \
     --add_loss_5=False \
     --prioritized_buffer=True \
@@ -32,7 +32,7 @@ for i in {3..4}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias0.0-add_loss_5=False-deter-top_factor=0.8 \
+    --exp_name=cplppo-cpl_bias0.25-add_loss_5=False-deter-top_factor=0.3 \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
@@ -40,12 +40,36 @@ do
     --free_level=0.95 \
     --use_chunk_adv=True \
     --num_steps_per_chunk=64 \
-    --cpl_bias=0.0 \
+    --cpl_bias=0.25 \
     --num_comparisons=-1 \
     --add_loss_5=False \
     --prioritized_buffer=True \
-    --top_factor=0.8 \
-    > "0501-add_loss_5=False-top_factor=0.8-seed${seeds[$i]}.log" 2>&1 &
+    --top_factor=0.3 \
+    > "0501-add_loss_5=False-top_factor=0.3-seed${seeds[$i]}.log" 2>&1 &
+done
+
+
+
+
+# Loop over each GPU
+for i in {3..4}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
+    --exp_name=cplppo-cpl_bias0.5-add_loss_5=False-deter-top_factor=0.3 \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.95 \
+    --use_chunk_adv=True \
+    --num_steps_per_chunk=64 \
+    --cpl_bias=0.5 \
+    --num_comparisons=-1 \
+    --add_loss_5=False \
+    --prioritized_buffer=True \
+    --top_factor=0.3 \
+    > "0501-add_loss_5=False-top_factor=0.3-cpl_bias=0.5-seed${seeds[$i]}.log" 2>&1 &
 done
 
 
