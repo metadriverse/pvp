2c0fd89a99bba6df9b379075118bc01c56dc5788
pengzhenghao <pzh@cs.ucla.edu>
2024-05-01 13:18:33 -0700

    add more

--- scripts/0430_cplppo.sh
+++ scripts/0430_cplppo.sh
@@ -4,30 +4,12 @@
 seeds=(0 100 200 300 400 500 600 700)
 
 
-## Loop over each GPU
-#for i in {2..4}
-#do
-#    CUDA_VISIBLE_DEVICES=$i \
-#    nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-#    --exp_name=cplppo-chunkadv-num_steps_per_chunk64-cpl_bias0.5-num_comparisons-1-doublecpl \
-#    --wandb \
-#    --wandb_project=pvp2024 \
-#    --wandb_team=drivingforce \
-#    --seed=${seeds[$i]} \
-#    --free_level=0.95 \
-#    --use_chunk_adv=True \
-#    --num_steps_per_chunk=64 \
-#    --cpl_bias=0.5 \
-#    --num_comparisons=-1 \
-#    > "0430-cplppo-use_chunk_adv-num_steps_per_chunk64-cpl_bias0.5-num_comparisons-1-seed${seeds[$i]}.log" 2>&1 &
-#done
-
 # Loop over each GPU
-for i in {0..2}
+for i in {0..1}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias0.0-add_loss_5=False-deter-top_factor=0.1 \
+    --exp_name=cplppo-cpl_bias0.0-add_loss_5=False-deter-top_factor=0.5 \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
@@ -39,18 +21,18 @@ do
     --num_comparisons=-1 \
     --add_loss_5=False \
     --prioritized_buffer=True \
-    --top_factor=0.1 \
-    > "0501-add_loss_5=False-top_factor=0.1-seed${seeds[$i]}.log" 2>&1 &
+    --top_factor=0.5 \
+    > "0501-add_loss_5=False-top_factor=0.5-seed${seeds[$i]}.log" 2>&1 &
 done
 
 
 
 # Loop over each GPU
-for i in {3..5}
+for i in {3..4}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias0.0-add_loss_5=False-deter-top_factor=0.2 \
+    --exp_name=cplppo-cpl_bias0.0-add_loss_5=False-deter-top_factor=0.8 \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
@@ -62,31 +44,8 @@ do
     --num_comparisons=-1 \
     --add_loss_5=False \
     --prioritized_buffer=True \
-    --top_factor=0.2 \
-    > "0501-add_loss_5=False-top_factor=0.2-seed${seeds[$i]}.log" 2>&1 &
+    --top_factor=0.8 \
+    > "0501-add_loss_5=False-top_factor=0.8-seed${seeds[$i]}.log" 2>&1 &
 done
 
 
-
-
-
-# Loop over each GPU
-for i in {4..7}
-do
-    CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias0.0-add_loss_5=False-deter-top_factor=0.3 \
-    --wandb \
-    --wandb_project=pvp2024 \
-    --wandb_team=drivingforce \
-    --seed=${seeds[$i]} \
-    --free_level=0.95 \
-    --use_chunk_adv=True \
-    --num_steps_per_chunk=64 \
-    --cpl_bias=0.0 \
-    --num_comparisons=-1 \
-    --add_loss_5=False \
-    --prioritized_buffer=True \
-    --top_factor=0.3 \
-    > "0501-add_loss_5=False-top_factor=0.3-seed${seeds[$i]}.log" 2>&1 &
-done
