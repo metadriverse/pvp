d369be149002fcd7be1b1f106f9037716816c1b6
pengzhenghao <pzh@cs.ucla.edu>
2024-05-03 17:00:39 -0700

    use 50 ep

--- pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py
+++ pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py
@@ -266,7 +266,7 @@ if __name__ == '__main__':
         # eval
         eval_env=eval_env,
         eval_freq=500,
-        n_eval_episodes=30,
+        n_eval_episodes=50,
         eval_log_path=str(trial_dir),
 
         # logging
