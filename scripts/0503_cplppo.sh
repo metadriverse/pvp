92872641c2b22cc2c0b5128fcfe257b6c6ff0aab
pengzhenghao <pzh@cs.ucla.edu>
2024-05-05 11:12:13 -0700

    add script

--- scripts/0503_cplppo.sh
+++ scripts/0503_cplppo.sh
@@ -9,35 +9,7 @@ for i in {4..5}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias=0.5-fixedstd0.0-addloss5 \
-    --wandb \
-    --wandb_project=pvp2024 \
-    --wandb_team=drivingforce \
-    --seed=${seeds[$i]} \
-    --free_level=0.95 \
-    --use_chunk_adv=True \
-    --num_steps_per_chunk=64 \
-    --cpl_bias=0.5 \
-    --num_comparisons=-1 \
-    --add_loss_5=True \
-    --prioritized_buffer=True \
-    --mask_same_actions=False \
-    --remove_loss_1=False \
-    --training_deterministic=True \
-    --use_target_policy=False \
-    --remove_loss_3=False \
-    --log_std_init=0.0 \
-    > "0503-exp5-seed${seeds[$i]}.log" 2>&1 &
-done
-
-
-
-# Loop over each GPU
-for i in {6..7}
-do
-    CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias=0.5-fixedstd0.0-remove_loss_6 \
+    --exp_name=cplppo-cpl_bias=0.5-fixedstd-1.0-remove_loss_6 \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
@@ -55,6 +27,34 @@ do
     --training_deterministic=True \
     --use_target_policy=False \
     --remove_loss_3=False \
-    --log_std_init=0.0 \
+    --log_std_init=-1.0 \
+    > "0503-exp15-seed${seeds[$i]}.log" 2>&1 &
+done
+
+
+# Loop over each GPU
+for i in {6..7}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
+    --exp_name=cplppo-cpl_bias=0.5-fixedstd-3.0-remove_loss_6 \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.95 \
+    --use_chunk_adv=True \
+    --num_steps_per_chunk=64 \
+    --cpl_bias=0.5 \
+    --num_comparisons=-1 \
+    --add_loss_5=False \
+    --prioritized_buffer=True \
+    --mask_same_actions=False \
+    --remove_loss_1=False \
+    --remove_loss_6=True \
+    --training_deterministic=True \
+    --use_target_policy=False \
+    --remove_loss_3=False \
+    --log_std_init=-3.0 \
     > "0503-exp15-seed${seeds[$i]}.log" 2>&1 &
 done
