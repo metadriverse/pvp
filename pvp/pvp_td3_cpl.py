3691ab17dcfe8ec445e9864bc88877a519baf643
pengzhenghao <pzh@cs.ucla.edu>
2024-05-05 15:03:37 -0700

    revert clamp

--- pvp/pvp_td3_cpl.py
+++ pvp/pvp_td3_cpl.py
@@ -821,7 +821,7 @@ class PVPRealTD3CPL(PVPTD3CPL):
         # FIXME
         # FIXME
         # FIXME
-        # actions_novice = actions_novice.clamp(-1, 1)
+        actions_novice = actions_novice.clamp(-1, 1)
 
         new_valid_mask = torch.stack(new_valid_mask).bool()
         new_valid_ep = torch.from_numpy(np.array(new_valid_ep)).to(obs.device)
