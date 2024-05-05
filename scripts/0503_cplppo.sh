80ec01a7579ce9c06242ef4db59f96d52df0536c
pengzhenghao <pzh@cs.ucla.edu>
2024-05-05 15:06:30 -0700

    add

--- scripts/0503_cplppo.sh
+++ scripts/0503_cplppo.sh
@@ -5,44 +5,16 @@ seeds=(0 100 200 300 400 500 600 700)
 
 
 # Loop over each GPU
-for i in {4..5}
+for i in {2..3}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias=0.5-remove_loss_6-remove_loss_1-clamp \
+    --exp_name=cplppo-cpl_bias=0.5-remove_loss_6-clamp \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
     --seed=${seeds[$i]} \
-    --free_level=0.95 \
-    --use_chunk_adv=True \
-    --num_steps_per_chunk=64 \
-    --cpl_bias=0.5 \
-    --num_comparisons=-1 \
-    --add_loss_5=False \
-    --prioritized_buffer=True \
-    --mask_same_actions=False \
-    --remove_loss_1=True \
-    --remove_loss_6=True \
-    --training_deterministic=True \
-    --use_target_policy=False \
-    --remove_loss_3=False \
-    > "0503-exp45-seed${seeds[$i]}.log" 2>&1 &
-done
-
-
-
-# Loop over each GPU
-for i in {6..7}
-do
-    CUDA_VISIBLE_DEVICES=$i \
-    nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias=0.5-remove_loss_6-regression-clamp \
-    --wandb \
-    --wandb_project=pvp2024 \
-    --wandb_team=drivingforce \
-    --seed=${seeds[$i]} \
-    --free_level=0.95 \
+    --free_level=0.5 \
     --use_chunk_adv=True \
     --num_steps_per_chunk=64 \
     --cpl_bias=0.5 \
@@ -55,5 +27,33 @@ do
     --training_deterministic=True \
     --use_target_policy=False \
     --remove_loss_3=False \
-    > "0503-exp4-seed${seeds[$i]}.log" 2>&1 &
+    > "0503-exp4225-seed${seeds[$i]}.log" 2>&1 &
+done
+
+
+
+# Loop over each GPU
+for i in {0..1}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
+    --exp_name=cplppo-cpl_bias=0.0-remove_loss_6-clamp \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.5 \
+    --use_chunk_adv=True \
+    --num_steps_per_chunk=64 \
+    --cpl_bias=0.0 \
+    --num_comparisons=-1 \
+    --add_loss_5=False \
+    --prioritized_buffer=True \
+    --mask_same_actions=False \
+    --remove_loss_1=False \
+    --remove_loss_6=True \
+    --training_deterministic=True \
+    --use_target_policy=False \
+    --remove_loss_3=False \
+    > "0503-exp42382-seed${seeds[$i]}.log" 2>&1 &
 done
