6b6a18a1e93c7c4522877c63f469cf171dfefd6c
pengzhenghao <pzh@cs.ucla.edu>
2024-05-05 16:39:42 -0700

    add script

--- scripts/0503_cplppo.sh
+++ scripts/0503_cplppo.sh
@@ -5,16 +5,16 @@ seeds=(0 100 200 300 400 500 600 700)
 
 
 # Loop over each GPU
-for i in {2..3}
+for i in {0..3}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias=0.5-remove_loss_6-clamp \
+    --exp_name=cplppo-cpl_bias=0.5-remove_loss_6-use_target_mask \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
     --seed=${seeds[$i]} \
-    --free_level=0.5 \
+    --free_level=0.95 \
     --use_chunk_adv=True \
     --num_steps_per_chunk=64 \
     --cpl_bias=0.5 \
@@ -25,27 +25,26 @@ do
     --remove_loss_1=False \
     --remove_loss_6=True \
     --training_deterministic=True \
-    --use_target_policy=False \
+    --use_target_policy_only_overwrite_takeover=True \
+    --use_target_policy=True \
     --remove_loss_3=False \
     > "0503-exp4225-seed${seeds[$i]}.log" 2>&1 &
 done
 
-
-
 # Loop over each GPU
-for i in {0..1}
+for i in {4..7}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias=0.0-remove_loss_6-clamp \
+    --exp_name=cplppo-cpl_bias=0.5-remove_loss_6-use_target_nomask \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
     --seed=${seeds[$i]} \
-    --free_level=0.5 \
+    --free_level=0.95 \
     --use_chunk_adv=True \
     --num_steps_per_chunk=64 \
-    --cpl_bias=0.0 \
+    --cpl_bias=0.5 \
     --num_comparisons=-1 \
     --add_loss_5=False \
     --prioritized_buffer=True \
@@ -53,7 +52,9 @@ do
     --remove_loss_1=False \
     --remove_loss_6=True \
     --training_deterministic=True \
+    --use_target_policy_only_overwrite_takeover=True \
     --use_target_policy=False \
     --remove_loss_3=False \
-    > "0503-exp42382-seed${seeds[$i]}.log" 2>&1 &
+    > "0503-exp422522-seed${seeds[$i]}.log" 2>&1 &
 done
+
