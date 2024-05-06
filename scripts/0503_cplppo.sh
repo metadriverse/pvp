821450513e3e3cd32443b74cc9fbf9bee5875ca8
pengzhenghao <pzh@cs.ucla.edu>
2024-05-06 15:40:45 -0700

    fix

--- scripts/0503_cplppo.sh
+++ scripts/0503_cplppo.sh
@@ -5,11 +5,11 @@ seeds=(0 100 200 300 400 500 600 700)
 
 
 # Loop over each GPU
-for i in {0..3}
+for i in {0..7}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias=0.5-discardfailedep \
+    --exp_name=cplppo-cpl_bias=0.5-discardfailedep-fixed \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
@@ -17,7 +17,7 @@ do
     --free_level=0.95 \
     --use_chunk_adv=True \
     --num_steps_per_chunk=64 \
-    --cpl_bias=0.0 \
+    --cpl_bias=0.5 \
     --num_comparisons=-1 \
     --add_loss_5=False \
     --prioritized_buffer=True \
