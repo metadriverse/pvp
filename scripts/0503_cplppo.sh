d9486b9ad7de8c2c6f91f3c0f12b159b59e906e4
pengzhenghao <pzh@cs.ucla.edu>
2024-05-05 22:18:33 -0700

    add script

--- scripts/0503_cplppo.sh
+++ scripts/0503_cplppo.sh
@@ -5,11 +5,11 @@ seeds=(0 100 200 300 400 500 600 700)
 
 
 # Loop over each GPU
-for i in {0..3}
+for i in {4..7}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias=0.5-remove_loss_6-remove_loss_1-regression \
+    --exp_name=cplppo-cpl_bias=0.5-remove_loss_6-newloss1 \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
@@ -22,11 +22,11 @@ do
     --add_loss_5=False \
     --prioritized_buffer=True \
     --mask_same_actions=False \
-    --remove_loss_1=True \
+    --remove_loss_1=False \
     --remove_loss_6=True \
     --training_deterministic=True \
     --use_target_policy_only_overwrite_takeover=False \
     --use_target_policy=False \
     --remove_loss_3=False \
-    > "0503-exp4225-seed${seeds[$i]}.log" 2>&1 &
+    > "0503-exp42-seed${seeds[$i]}.log" 2>&1 &
 done
