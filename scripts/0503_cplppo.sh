6e0bac88ceb6a5b9e979593e2f2e168d726474f1
pengzhenghao <pzh@cs.ucla.edu>
2024-05-06 20:38:37 -0700

    add

--- scripts/0503_cplppo.sh
+++ scripts/0503_cplppo.sh
@@ -35,11 +35,11 @@ done
 
 
 # Loop over each GPU
-for i in {2..3}
+for i in {4..5}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
-    --exp_name=cplppo-cpl_bias=0.5-loss1_cpl_bias=1.0-bc_loss_weight2 \
+    --exp_name=cplppo-cpl_bias=0.5-loss1_cpl_bias=1.0-bc_loss_weight5 \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
@@ -58,6 +58,36 @@ do
     --use_target_policy_only_overwrite_takeover=False \
     --use_target_policy=False \
     --remove_loss_3=False \
-    --bc_loss_weight=2.0 \
-    > "0503-exp23-seed${seeds[$i]}.log" 2>&1 &
+    --bc_loss_weight=5.0 \
+    > "0503-exp23555-seed${seeds[$i]}.log" 2>&1 &
+done
+
+
+
+# Loop over each GPU
+for i in {6..7}
+do
+    CUDA_VISIBLE_DEVICES=$i \
+    nohup python pvp/experiments/metadrive/train_td3cpl_metadrive_fakehuman.py \
+    --exp_name=cplppo-cpl_bias=0.5-loss1_cpl_bias=1.0-bc_loss_weight10 \
+    --wandb \
+    --wandb_project=pvp2024 \
+    --wandb_team=drivingforce \
+    --seed=${seeds[$i]} \
+    --free_level=0.95 \
+    --use_chunk_adv=True \
+    --num_steps_per_chunk=64 \
+    --cpl_bias=0.5 \
+    --num_comparisons=-1 \
+    --add_loss_5=False \
+    --prioritized_buffer=True \
+    --mask_same_actions=False \
+    --remove_loss_1=False \
+    --remove_loss_6=True \
+    --training_deterministic=True \
+    --use_target_policy_only_overwrite_takeover=False \
+    --use_target_policy=False \
+    --remove_loss_3=False \
+    --bc_loss_weight=10.0 \
+    > "0503-exp2310-seed${seeds[$i]}.log" 2>&1 &
 done
