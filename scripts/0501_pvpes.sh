5eb62e6761eaa07d33c0955cbd2cfedcbd74624d
pengzhenghao <pzh@cs.ucla.edu>
2024-05-07 19:15:48 -0700

    add a script to test remove negative

--- scripts/0501_pvpes.sh
+++ scripts/0501_pvpes.sh
@@ -5,11 +5,11 @@ seeds=(0 100 200 300 400 500 600 700)
 
 
 # Loop over each GPU
-for i in {0..3}
+for i in {3..5}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python pvp/experiments/metadrive/train_pvpes_metadrive_fakehuman.py \
-    --exp_name=pvpes-metadrive-regression \
+    --exp_name=pvpes-metadrive-regression-remove_negative \
     --wandb \
     --wandb_project=pvp2024 \
     --wandb_team=drivingforce \
@@ -21,7 +21,7 @@ do
     --reward_1_for_all=False \
     --reward_0_for_negative=False \
     --use_weighted_reward=False \
-    --remove_negative=False \
+    --remove_negative=True \
     --adaptive_batch_size=True \
     --agent_data_ratio=1.0 \
     > "0501-agent_data_ratio=1.0-seed${seeds[$i]}.log" 2>&1 &
